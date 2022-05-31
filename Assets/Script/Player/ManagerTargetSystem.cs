1eb008e1e9bfcf8b376120d57aa8c414c891e412
Ruffo Maximiliano <36495549+SlayerWx@users.noreply.github.com>
2022-05-31 17:06:50 -0300

    basic TargetSystem Test based in the distance

---
+++ Assets/Script/Player/ManagerTargetSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerTargetSystem : MonoBehaviour
+{
+    Transform actualTarget;
+    Material actualTestMat;
+    public List<Transform> enemyTest;
+    public List<Material> matTest;
+    public Transform player;
+    void Start()
+    {
+        foreach(Transform aEnemy in enemyTest)
+        {
+            matTest.Add(aEnemy.gameObject.GetComponent<MeshRenderer>().material);
+
+        }
+        actualTarget = enemyTest[0];
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(actualTestMat != null)
+        actualTestMat.color = Color.magenta;
+        for (int i = 0; i < enemyTest.Count;i++)
+        {
+            if(Vector3.Distance(player.position,actualTarget.position) > Vector3.Distance(player.position, enemyTest[i].position))
+            {
+                actualTarget = enemyTest[i];
+                actualTestMat = matTest[i]; //test
+            }
+        }
+
+        actualTestMat.color = Color.red; //test
+    }
+}
